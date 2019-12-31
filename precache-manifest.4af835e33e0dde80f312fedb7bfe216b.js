d23e0dead83cd1b897ad44e457d50204e72a634e
Michael Dunlap <michaeldunlap@earthlink.net>
2019-12-31 11:20:05 -0800

    Updates

---
+++ precache-manifest.4af835e33e0dde80f312fedb7bfe216b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6d808bc54fb61562db19475e7372a0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "469ef7b165c448092029",
+    "url": "/monsters-rolodex/static/css/main.f796f825.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "469ef7b165c448092029",
+    "url": "/monsters-rolodex/static/js/main.646661b6.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
